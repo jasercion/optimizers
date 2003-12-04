5debd48609123a3af7db9946c37e16a682c5e428
pln <>
2003-12-04 06:54:36 +0000

    Throw exeption on dpptri errors. Prevent roundoff error in square roots.

--- src/Optimizer.cxx
+++ src/Optimizer.cxx
@@ -22,7 +22,7 @@ std::vector<double> & Optimizer::getUncertainty(void) {
    choleskyDecompose(hess);
 
 // Repack in compact form.
-   int npars = static_cast<int>(sqrt(hess.size()));
+   int npars = static_cast<int>(sqrt(hess.size())+0.1);
    std::valarray<double> compactHess(npars*(npars+1)/2);
    int indx(0);
    for (int j = 0; j < npars; j++) {
@@ -35,6 +35,11 @@ std::vector<double> & Optimizer::getUncertainty(void) {
    int info;
    const char uplo = 'U';
    dpptri_(&uplo, &npars, &compactHess[0], &info, 1);
+   if (info < 0)
+      throw Exception("DPPTRI: illegal argument value", -info);
+   else if (info > 0)
+      throw Exception("DPPTRI: Zero diagonal element in Cholesky factor",
+                      info);
 
 // Extract the error estimates as the square-roots of the diagonal
 // elements.
@@ -84,7 +89,7 @@ void Optimizer::computeHessian(std::valarray<double> &hess, double eps) {
 void Optimizer::choleskyDecompose(std::valarray<double> &array) {
 // This implementation is based on NR's choldc().
 
-   int npts = static_cast<int>(sqrt(array.size()));
+   int npts = static_cast<int>(sqrt(array.size())+0.1);
    std::valarray<double> p(npts);
 
    for (int i = 0; i < npts; i++) {
