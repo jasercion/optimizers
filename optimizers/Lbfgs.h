3ba36a6882f92aa8b985ed7e2e08fc337672a8a0
jchiang <>
2006-05-11 05:05:32 +0000

    set m_pgtol to a demonstrably better value

--- optimizers/Lbfgs.h
+++ optimizers/Lbfgs.h
@@ -59,7 +59,7 @@ namespace optimizers {
     
       Lbfgs(Statistic &stat) 
          : Optimizer(stat), m_maxVarMetCorr(5),
-           m_maxIterations(100), m_pgtol(1e-7), m_retCode(0) {}
+           m_maxIterations(100), m_pgtol(0), m_retCode(0) {}
     
     virtual ~Lbfgs() {}
     
