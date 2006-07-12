4ad842345c5a21b324acd3158b31245eaebfd0ef
jchiang <>
2006-07-12 02:11:38 +0000

    refactor from singleton

--- optimizers/Amoeba.h
+++ optimizers/Amoeba.h
@@ -9,10 +9,6 @@
 #ifndef optimizers_Amoeba_h
 #define optimizers_Amoeba_h
 
-#include <iostream>
-#include <stdexcept>
-#include <string>
-
 #include "optimizers/Functor.h"
 
 namespace optimizers {
@@ -24,10 +20,6 @@ namespace optimizers {
  * std::vector<double> of parameter values using the Nelder-Mead
  * algorithm implemented in NR.
  *
- * Since the functor is stored as static data member, which is
- * necessary to interface to the NR amoeba routine, only one instance
- * of this object can be created, hence it is itself Singleton.
- *
  * @author J. Chiang
  *
  * $Header$
@@ -37,78 +29,29 @@ class Amoeba {
 
 public:
 
-   static Amoeba * 
-   instance(Functor &functor, 
-            std::vector<std::vector<double> > & startingSimplex) {
-      if (s_instance == 0) {
-         s_instance = new Amoeba(functor, startingSimplex);
-      } else {
-         throw std::invalid_argument("A functor already exists. "
-                                     + std::string("Delete the Singleton ")
-                                     + "instance first before attempting to "
-                                     + std::string("minimize a different ")
-                                     + "function object.");
-      }
-      return s_instance;
-   }
-
-   static Amoeba * instance() {
-      return s_instance;
+   Amoeba(Functor & functor, const std::vector<double> & params) 
+      : m_functor(functor), m_npars(params.size()) {
+      buildSimplex(params);
    }
 
    ~Amoeba() {}
 
-   static void delete_instance() {
-      delete s_instance;
-      s_instance = 0;
-   }
-
    /// @return The estimate of the minimum value of the functor.
    /// @param params The parameter values at the minimum estimate.
    /// @param tol The absolute tolerance for convergence.
-   double findMin(std::vector<double> & params, double tol = 1e-15);
-
-   /// @param params An ndim vector of parameter values as a candidate
-   /// starting point for the minimization.
-   /// @param simplex The ndim+1 by ndim vector as the starting simplex
-   /// constructed by looping through each dimension and setting the
-   /// parameter value to (1+frac) times the input value.
-   /// @param frac This sets the size of the initial simplex.
-   static void buildStartingSimplex(const std::vector<double> & params,
-                                    std::vector<std::vector<double> > &simplex,
-                                    double frac = 0.1, bool addfrac = false);
-
-protected:
-
-   /// @param functor The function object to be minimized.  It must
-   /// take as an argument a std::vector<double>, representing the
-   /// parameter values at which it is to be evaluated.
-   /// @param startingSimplex Effectively an ndim+1 by ndim array
-   /// containing the vertices of the starting simplex.
-   Amoeba(Functor &functor,
-          std::vector<std::vector<double> > & startingSimplex) {
-      s_functor = &functor;
-      s_startingSimplex = startingSimplex;
-      s_npars = startingSimplex.size() - 1;
-   }
+   double findMin(std::vector<double> & params, double tol=1e-15);
 
 private:
 
-   static Amoeba * s_instance;
+   Functor & m_functor;
+   size_t m_npars;
+   std::vector< std::vector<double> > m_simplex;
 
-   static Functor * s_functor;
-
-   static std::vector<std::vector<double> > s_startingSimplex;
-   
-   static int s_npars;
-
-   static double func(double *x) {
-      std::vector<double> xvals;
-      for (int i = 1; i < s_npars+1; i++) {
-         xvals.push_back(x[i]);
-      }
-      return (*s_functor)(xvals);
-   }
+   /// @param params An ndim vector of parameter values as a candidate
+   /// starting point for the minimization.
+   /// @param frac This sets the size of the initial simplex.
+   void buildSimplex(const std::vector<double> & params,
+                     double frac=0.1, bool addfrac=false);
 
 };
 
--- src/Amoeba.cxx
+++ src/Amoeba.cxx
@@ -7,177 +7,147 @@
  */
 
 #include <cmath>
+
 #include <iostream>
+#include <stdexcept>
 
 #include "optimizers/Amoeba.h"
 
 namespace {
-#define NR_END 1
-#define FREE_ARG char*
 
-void nrerror(char * error_text)
-/* Numerical Recipes standard error handler */
-{
-	fprintf(stderr,"Numerical Recipes run-time error...\n");
-	fprintf(stderr,"%s\n",error_text);
-	fprintf(stderr,"...now exiting to system...\n");
-	exit(1);
+double amotry(std::vector< std::vector<double> > & p,
+              std::vector<double> & y,
+              std::vector<double> & psum,
+              int ndim, optimizers::Functor & func, int ihi, double fac) {
+   std::vector<double> ptry(ndim, 0);
+   double fac1((1. - fac)/float(ndim));
+   double fac2(fac1 - fac);
+   for (int j = 0; j < ndim; j++) {
+      ptry.at(j) = psum.at(j)*fac1 - p.at(ihi).at(j)*fac2;
+   }
+   double ytry(func(ptry));
+   if (ytry < y.at(ihi)) {
+      y.at(ihi) = ytry;
+      for (int j = 0; j < ndim; j++) {
+         psum.at(j) += ptry.at(j) - p.at(ihi).at(j);
+         p.at(ihi).at(j) = ptry.at(j);
+      }
+   }
+   return ytry;
 }
 
-double *vector(long nl, long nh)
-/* allocate a double vector with subscript range v[nl..nh] */
-{
-	double *v;
-
-	v=(double *)malloc((unsigned int) ((nh-nl+1+NR_END)*sizeof(double)));
-	if (!v) nrerror("allocation failure in vector()");
-	return v-nl+NR_END;
+void swap(double & a, double & b) {
+   double tmp(a);
+   a = b;
+   b = tmp;
 }
 
-void free_vector(double *v, long nl, long nh)
-/* free a double vector allocated with vector() */
-{
-	free((FREE_ARG) (v+nl-NR_END));
+void amoeba(std::vector< std::vector<double> > & p,
+            std::vector<double> & y, int ndim, double ftol,
+            optimizers::Functor & func, int & nfunc) {
+   static int nmax(5000);
+   int mpts(ndim + 1);
+   std::vector<double> psum(ndim, 0);
+   nfunc = 0;
+   for (int j = 0; j < ndim; j++) {
+      double sum(0);
+      for (int i = 0; i < mpts; i++) {
+         sum += p.at(i).at(j);
+      }
+      psum.at(j) = sum;
+   }
+   while(true) {
+      int ilo(0);
+      int inhi;
+      int ihi = y.at(0) > y.at(1) ? (inhi=1,0) : (inhi=0,1);
+      for (int i = 0; i < mpts; i++) {
+         if (y.at(i) <= y.at(ilo)) {
+            ilo = i;
+         }
+         if (y.at(i) > y.at(ihi)) {
+            inhi = ihi;
+            ihi = i;
+         } else if (y.at(i) > y.at(inhi) && i != ihi) {
+            inhi = i;
+         }
+      }
+      double rtol(2.0*std::fabs(y.at(ihi) - y.at(ilo))
+                  /(std::fabs(y.at(ihi)) + std::fabs(y.at(ilo))));
+      if (rtol < ftol) {
+         swap(y.at(0), y.at(ilo));
+         for (int i = 0; i < ndim; i++) {
+            swap(p.at(0).at(i), p.at(ilo).at(i));
+         }
+         break;
+      }
+      if (nfunc >= nmax) {
+         throw std::runtime_error("amoeba: nmax exceeded");
+      }
+      nfunc += 2;
+      double ytry(amotry(p, y, psum, ndim, func, ihi, 0.5));
+      if (ytry <= y.at(ilo)) {
+         ytry = amotry(p, y, psum, ndim, func, ihi, 2.0);
+      } else if (ytry >= y.at(inhi)) {
+         double ysave(y.at(ihi));
+         ytry = amotry(p, y, psum, ndim, func, ihi, 0.5);
+         if (ytry >= ysave) {
+            for (int i = 0; i < mpts; i++) {
+               if (i != ilo) {
+                  for (int j = 0; j < ndim; j++) {
+                     p.at(i).at(j) = psum.at(j) = 
+                        0.5*(p.at(i).at(j) + p.at(ilo).at(j));
+                  }
+                  y.at(i) = func(psum);
+               }
+            }
+            nfunc += ndim;
+            for (int j = 0; j < ndim; j++) {
+               double sum(0);
+               for (int i = 0; i < mpts; i++) {
+                  sum += p.at(i).at(j);
+               }
+               psum.at(j) = sum;
+            }
+         } else {
+            --nfunc;
+         }
+      }
+   }
 }
-#undef FREE_ARG
-#undef NR_END
-
-#define NMAX 5000
-#define GET_PSUM \
-		for (j=1;j<=ndim;j++) {\
-		for (sum=0.0,i=1;i<=mpts;i++) sum += p[i][j];\
-		psum[j]=sum;}
-#define SWAP(a,b) {swap=(a);(a)=(b);(b)=swap;}
-
-double amotry(double **p, double *y, double *psum, int ndim, 
-             double (*funk)(double *), int ihi, double fac)
-{
-	int j;
-	double fac1,fac2,ytry,*ptry;
-
-	ptry=vector(1,ndim);
-	fac1=(1.0-fac)/ndim;
-	fac2=fac1-fac;
-	for (j=1;j<=ndim;j++) ptry[j]=psum[j]*fac1-p[ihi][j]*fac2;
-	ytry=(*funk)(ptry);
-	if (ytry < y[ihi]) {
-		y[ihi]=ytry;
-		for (j=1;j<=ndim;j++) {
-			psum[j] += ptry[j]-p[ihi][j];
-			p[ihi][j]=ptry[j];
-		}
-	}
-	free_vector(ptry,1,ndim);
-	return ytry;
-}
-/* (C) Copr. 1986-92 Numerical Recipes Software 0@.1Y.. */
-
-void amoeba(double **p, double y[], int ndim, double ftol, 
-            double (*funk)(double *), int *nfunk)
-{
-/* 	double amotry(); */
-	int i,ihi,ilo,inhi,j,mpts=ndim+1;
-	double rtol,sum,swap,ysave,ytry,*psum;
-
-	psum=vector(1,ndim);
-	*nfunk=0;
-        for (j=1;j<=ndim;j++) {
-           for (sum=0.0,i=1;i<=mpts;i++) sum += p[i][j];
-           psum[j]=sum;
-        }
-	for (;;) {
-		ilo=1;
-		ihi = y[1]>y[2] ? (inhi=2,1) : (inhi=1,2);
-		for (i=1;i<=mpts;i++) {
-			if (y[i] <= y[ilo]) ilo=i;
-			if (y[i] > y[ihi]) {
-				inhi=ihi;
-				ihi=i;
-			} else if (y[i] > y[inhi] && i != ihi) inhi=i;
-		}
-		rtol=2.0*fabs(y[ihi]-y[ilo])/(fabs(y[ihi])+fabs(y[ilo]));
-		if (rtol < ftol) {
-			SWAP(y[1],y[ilo])
-			for (i=1;i<=ndim;i++) SWAP(p[1][i],p[ilo][i])
-			break;
-		}
-		if (*nfunk >= NMAX) nrerror("NMAX exceeded");
-		*nfunk += 2;
-		ytry=amotry(p,y,psum,ndim,funk,ihi,-1.0);
-		if (ytry <= y[ilo])
-			ytry=amotry(p,y,psum,ndim,funk,ihi,2.0);
-		else if (ytry >= y[inhi]) {
-			ysave=y[ihi];
-			ytry=amotry(p,y,psum,ndim,funk,ihi,0.5);
-			if (ytry >= ysave) {
-				for (i=1;i<=mpts;i++) {
-					if (i != ilo) {
-						for (j=1;j<=ndim;j++)
-							p[i][j]=psum[j]=0.5*(p[i][j]+p[ilo][j]);
-						y[i]=(*funk)(psum);
-					}
-				}
-				*nfunk += ndim;
-                                for (j=1;j<=ndim;j++) {
-                                   for (sum=0.0,i=1;i<=mpts;i++) 
-                                      sum += p[i][j];
-                                   psum[j]=sum;
-                                }
-			}
-		} else --(*nfunk);
-	}
-	free_vector(psum,1,ndim);
-}
-#undef SWAP
-#undef GET_PSUM
-#undef NMAX
-/* (C) Copr. 1986-92 Numerical Recipes Software 0@.1Y.. */
 
 } // anonymous namespace
 
 namespace optimizers {
 
-Amoeba * Amoeba::s_instance = 0;
-Functor * Amoeba::s_functor = 0;
-std::vector<std::vector<double> > Amoeba::s_startingSimplex;
-int Amoeba::s_npars;
-
 double Amoeba::findMin(std::vector<double> & params, double tol) {
    std::vector<double> yvalues;
-   yvalues.reserve(s_npars + 1);
-   for (int i = 0; i < s_npars + 1; i++) {
-      double yval = (*s_functor)(s_startingSimplex[i]);
+   yvalues.reserve(m_npars + 1);
+   for (size_t i = 0; i < m_npars + 1; i++) {
+      double yval(m_functor(m_simplex.at(i)));
       yvalues.push_back(yval);
    }
-   int nevals;
-   std::vector<double*> my_simplex;
-   my_simplex.reserve(s_npars + 1);
-   for (int i = 0; i < s_npars + 1; i++) {
-      my_simplex.push_back(&s_startingSimplex[i][0]-1);
-   }
-   ::amoeba(&my_simplex[0]-1, &yvalues[0]-1, s_npars, tol, &func, &nevals);
-   int imin = 0;
-   double ymin = yvalues[imin];
-   for (int i = 1; i < s_npars + 1; i++) {
-      if (yvalues[imin] < ymin) {
+   int nevals(0);
+   ::amoeba(m_simplex, yvalues, m_npars, tol, m_functor, nevals);
+   int imin(0);
+   double ymin(yvalues.at(imin));
+   for (size_t i = 1; i < m_npars + 1; i++) {
+      if (yvalues.at(imin) < ymin) {
          imin = i;
-         ymin = yvalues[imin];
+         ymin = yvalues.at(imin);
       }
    }
-   params = s_startingSimplex[imin];
+   params = m_simplex.at(imin);
    return ymin;
 }
 
-void Amoeba::buildStartingSimplex(const std::vector<double> & params,
-                                  std::vector<std::vector<double> > & simplex,
-                                  double frac, bool addfrac) {
-   int npars = params.size();
-   simplex.clear();
-   simplex.reserve(npars);
-   simplex.push_back(params);
-   for (int i = 1; i < npars+1; i++) {
-      simplex.push_back(params);
-      double & value = simplex.back()[i-1];
+void Amoeba::buildSimplex(const std::vector<double> & params,
+                          double frac, bool addfrac) {
+   m_simplex.clear();
+   m_simplex.reserve(m_npars);
+   m_simplex.push_back(params);
+   for (size_t i = 1; i < m_npars+1; i++) {
+      m_simplex.push_back(params);
+      double & value(m_simplex.back()[i-1]);
       if (addfrac) {
          value += frac;
       } else {
