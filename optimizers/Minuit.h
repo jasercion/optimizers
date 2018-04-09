a8d49c230eb0e0ac815f3e3139ac750690c40c2b
jasercion <jasercion.gwu@gmail.com>
2018-04-09 16:52:22 -0400

    Update Minuit.h

--- optimizers/Minuit.h
+++ optimizers/Minuit.h
@@ -9,6 +9,7 @@
 #define optimizers_MINUIT_H
 
 #include "root/TDataType.h"
+#include "root/TDictionary.h"
 #include "root/TMinuit.h"
 #include "optimizers/Optimizer.h"
 #include "optimizers/Statistic.h"
@@ -123,7 +124,7 @@ namespace optimizers {
 //extern "C" {
   //! Initialize Minuit with I/O unit numbers for in, out, save
   //void Minuit::mninit(const integer*, const integer*, const integer*);
-  void TMinuit::mninit(TDataType::Int_t, TDataType::Int_t, TDataType::Int_t);
+  void TMinuit::mninit(TDictionary::Int_t, TDictionary::Int_t, TDictionary::Int_t);
   //! Define a parameter, assigning values and bounds
   void Minuit::mnparm(integer *  num, const char * chnam, double * stval, 
 	        double * step,  double * bnd1 , 
